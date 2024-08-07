1d33d1a7b34b5bef3e6b070c7c5ff011e80550ef
Krum Kadiyski <krum.kadiyski@gmail.com>
2024-08-07 12:15:06 +0300

    feature: add supabse and cleanup

--- client/src/api/supabase.js
+++ client/src/api/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_PROJECT_URL,
@@ -7,15 +7,32 @@ export const supabase = createClient(
 
 export const uploadFile = async ({ username, file, type }) => {
   const randomUUID = globalThis.crypto.randomUUID();
-  const ext = file.name.split('.').pop();
+  const ext = file.name.split(".").pop();
   const fileName = `${randomUUID}.${ext}`;
 
   const { data, error } = await supabase.storage
-    .from('models')
+    .from(import.meta.env.VITE_S3_BUCKET_NAME)
     .upload(`${username}/${type}/${fileName}`, file, {
-      cacheControl: '3600',
+      cacheControl: "3600",
       upsert: false,
     });
+
+  return { data, error };
+};
+
+export const deleteFiles = async (files) => {
+  const filePathsToDelete = files.map((file) => {
+    const [, pathFromModel] = file.url.split(
+      import.meta.env.VITE_S3_BUCKET_NAME
+    );
+
+    const pathWithoutSlash = pathFromModel.slice(1);
+
+    return pathWithoutSlash;
+  });
+  const { data, error } = await supabase.storage
+    .from(import.meta.env.VITE_S3_BUCKET_NAME)
+    .remove(filePathsToDelete);
 
   return { data, error };
 };
--- client/src/components/app-bar/logo.jsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from '@mui/material';
-
-const Logo = () => (
-  <Typography noWrap variant="h6" component="div" sx={{ display: { xs: 'none', sm: 'block' } }}>
-    3D Model Hub
-  </Typography>
-);
-
-export default Logo;
